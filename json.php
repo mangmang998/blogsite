86ad51bd36e6baea6a8969bd08eb1d77ccdcbd1f
yomtaaa <1517926014@qq.com>
2017-11-06 12:57:09 +0800

    logs

---
+++ json.php
@@ -0,0 +1,15 @@
+<?php
+	$data=array(
+			'title'=> 'HTML竞赛正式报名',
+			'date'=> '2017年11月08日~2017年12月30日',
+			'adress'=> '广州市 从化区 广州城建职业学院',
+			'number'=> '二教405',
+			'peopleSum'=> '100',
+			'people'=> '何佩华',
+			'OFF'=> true,
+			'msgBtn'=> '立即报名'
+		);		
+
+	echo json_encode($data);
+
+?>
